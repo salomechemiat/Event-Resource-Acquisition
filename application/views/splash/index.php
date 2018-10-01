a5baee52295c2e3313cf5a4843bf6487681fc3f1
lorkeylorna <38747291+lorkeylorna@users.noreply.github.com>
2018-10-01 20:42:49 +0300

    Updated Interface

--- application/views/splash/index.php
+++ application/views/splash/index.php
@@ -180,75 +180,75 @@
 			</div>
 			<div class="row">
 				<div class="col-lg-4 col-md-4 col-sm-6">
-					<a href="images/img_2.jpg" class="fh5co-project-item image-popup">
+					<a href="images/sound.jpg" class="fh5co-project-item image-popup">
 						<figure>
 							<div class="overlay"><i class="ti-plus"></i></div>
-							<img src="images/img_2.jpg" alt="Image" class="img-responsive">
+							<img src="images/sound.jpg" alt="Image" class="img-responsive">
 						</figure>
 						<div class="fh5co-text">
-							<h2>Constructive heading</h2>
-							<p>Far far away, behind the word mountains, far from the countries Vokalia..</p>
+							<h2>Sound System</h2>
+							<p>Get the sound system of your choice.. Sound never disappoints.</p>
 						</div>
 					</a>
 				</div>
 				<div class="col-lg-4 col-md-4 col-sm-6">
-					<a href="images/img_3.jpg" class="fh5co-project-item image-popup">
+					<a href="images/food.jpg" class="fh5co-project-item image-popup">
 						<figure>
 							<div class="overlay"><i class="ti-plus"></i></div>
-							<img src="images/img_3.jpg" alt="Image" class="img-responsive">
+							<img src="images/food.jpg" alt="Image" class="img-responsive">
 						</figure>
 						<div class="fh5co-text">
-							<h2>Constructive heading</h2>
-							<p>Far far away, behind the word mountains, far from the countries Vokalia..</p>
+							<h2>Food</h2>
+							<p>We garra eat... We garra send the food you know..</p>
 						</div>
 					</a>
 				</div>
 				<div class="col-lg-4 col-md-4 col-sm-6">
-					<a href="images/img_4.jpg" class="fh5co-project-item image-popup">
+					<a href="images/avicii.jpg" class="fh5co-project-item image-popup">
 						<figure>
 							<div class="overlay"><i class="ti-plus"></i></div>
-							<img src="images/img_4.jpg" alt="Image" class="img-responsive">
+							<img src="images/avicii.jpg" alt="Image" class="img-responsive">
 						</figure>
 						<div class="fh5co-text">
-							<h2>Constructive heading</h2>
-							<p>Far far away, behind the word mountains, far from the countries Vokalia..</p>
+							<h2>DJ</h2>
+							<p>Turn the music up... Let's dance to the beat...</p>
 						</div>
 					</a>
 				</div>
 
 				<div class="col-lg-4 col-md-4 col-sm-6">
-					<a href="images/img_1.jpg" class="fh5co-project-item image-popup">
+					<a href="images/lights.jpg" class="fh5co-project-item image-popup">
 						<figure>
 							<div class="overlay"><i class="ti-plus"></i></div>
-							<img src="images/img_1.jpg" alt="Image" class="img-responsive">
+							<img src="images/lights.jpg" alt="Image" class="img-responsive">
 						</figure>
 						<div class="fh5co-text">
-							<h2>Constructive heading</h2>
-							<p>Far far away, behind the word mountains, far from the countries Vokalia..</p>
+							<h2>Lights</h2>
+							<p>What is a party without the lights?</p>
 						</div>
 					</a>
 				</div>
 				<div class="col-lg-4 col-md-4 col-sm-6">
-					<a href="images/img_5.jpg" class="fh5co-project-item image-popup">
+					<a href="images/drinks.jpg" class="fh5co-project-item image-popup">
 						<figure>
 							<div class="overlay"><i class="ti-plus"></i></div>
-							<img src="images/img_5.jpg" alt="Image" class="img-responsive">
+							<img src="images/drinks.jpg" alt="Image" class="img-responsive">
 						</figure>
 						<div class="fh5co-text">
-							<h2>Constructive heading</h2>
-							<p>Far far away, behind the word mountains, far from the countries Vokalia..</p>
+							<h2>Drinks</h2>
+							<p>Drink up to more life... We got the supplies</p>
 						</div>
 					</a>
 				</div>
 				<div class="col-lg-4 col-md-4 col-sm-6">
-					<a href="images/img_6.jpg" class="fh5co-project-item image-popup">
+					<a href="images/photography.jpg" class="fh5co-project-item image-popup">
 						<figure>
 							<div class="overlay"><i class="ti-plus"></i></div>
-							<img src="images/img_6.jpg" alt="Image" class="img-responsive">
+							<img src="images/photography.jpg" alt="Image" class="img-responsive">
 						</figure>
 						<div class="fh5co-text">
-							<h2>Constructive heading</h2>
-							<p>Far far away, behind the word mountains, far from the countries Vokalia..</p>
+							<h2>Photography and Videography</h2>
+							<p>Let us make memories</p>
 						</div>
 					</a>
 				</div>
